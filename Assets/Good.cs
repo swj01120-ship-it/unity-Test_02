2266339c0d6430d4468c2db1fbb10076dc0372b1
swj01120-ship-it <swj01120@gmail.com>
2025-10-22 15:30:18 +0900

    업데이트 최종

--- Assets/Good.cs
+++ Assets/Good.cs
@@ -9,15 +9,31 @@ public class Good : MonoBehaviour
     {
         Debug.Log("Good");
         Debug.Log("사이사이");
+        Debug.Log("empty");
+
+
         Debug.Log("Nice");
         Debug.Log("사이사이");
+
+
+        Debug.Log("empty");
+
+
         Debug.Log("happy");
         Debug.Log("안녕푸바오");
+
+        Debug.Log("empty");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("empty");
+
+
+        Debug.Log("empty");
+
+
+        Debug.Log("empty");
     }
 }
