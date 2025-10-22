4dfb5014f7a056e65fc4ac25c563d8a414221cdb
swj01120-ship-it <swj01120@gmail.com>
2025-10-22 15:00:47 +0900

    없네용브랜치

--- Assets/Branch.cs
+++ Assets/Branch.cs
@@ -7,7 +7,7 @@ public class Branch : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("아무것도없네용");
     }
 
     // Update is called once per frame
