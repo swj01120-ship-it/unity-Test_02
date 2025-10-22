0e314deb2aa23ecf7e8abe9ed7e9233651a8f05a
swj01120-ship-it <swj01120@gmail.com>
2025-10-22 14:59:32 +0900

    브랜찌

--- Assets/Branch.cs
+++ Assets/Branch.cs
@@ -7,7 +7,7 @@ public class Branch : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("요거는 브랜치");
     }
 
     // Update is called once per frame
