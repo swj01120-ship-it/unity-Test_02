90ef9b6daefeb4fe67c3928a061171e524f187ff
swj01120-ship-it <swj01120@gmail.com>
2025-10-22 15:07:25 +0900

    합병했어용

--- Assets/Branch.cs
+++ Assets/Branch.cs
@@ -7,12 +7,9 @@ public class Branch : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-//<<<<<<< HEAD
         Debug.Log("아무것도없네용");
-//=======
         Debug.Log("요거는 브랜치");
-//>>>>>>> He
-    }
+      }
 
     // Update is called once per frame
     void Update()
