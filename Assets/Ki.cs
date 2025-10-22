3390cde23646f08f82a6386f88233d0b02cf166b
swj01120-ship-it <swj01120@gmail.com>
2025-10-22 16:02:15 +0900

    그레이 추가요

--- Assets/Ki.cs
+++ Assets/Ki.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Ki : MonoBehaviour
 {
+    SpriteRenderer mySR;
     // Start is called before the first frame update
     void Start()
     {
+        mySR = GetComponent<SpriteRenderer>();  
         Debug.Log("Ki");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            change_color();
+        }
+    }
+    void change_color ()
+    {
+        mySR.color = Color.gray;
     }
 }
