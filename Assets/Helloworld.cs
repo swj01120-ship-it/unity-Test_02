9e57acd83273352baf55c1a24e155cdc862b1ae1
swj01120-ship-it <swj01120@gmail.com>
2025-10-22 10:59:10 +0900

    업데이트!

--- Assets/Helloworld.cs
+++ Assets/Helloworld.cs
@@ -7,7 +7,7 @@ public class Helloworld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Çï·Î¿ì");
     }
 
     // Update is called once per frame
