28a3383660ba9c35b5dd8a7df5bbc9da3b7b4382
swj01120-ship-it <swj01120@gmail.com>
2025-10-22 16:41:04 +0900

    업데이트

--- Assets/Branch.cs
+++ Assets/Branch.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class Branch : MonoBehaviour
 {
+    SpriteRenderer mySR;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("아무것도없네용");
-        Debug.Log("요거는 브랜치");
+        mySR = GetComponent<SpriteRenderer>();
       }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            change_color();
+        }
         
+    }
+    IEnumerator change_color()
+    {
+        Color myColor = new Color(0.001f, 0.001f, 0.001f, 0.0f);
+        //mySR.color = Color.clear;
+        while(true)
+        {
+            if (mySR.color.r == 0)
+            {
+                Debug.Log("에볼루션");
+                yield break;
+            }
+            mySR.color -= myColor;
+            yield return null;
+        }
+       
     }
 }
