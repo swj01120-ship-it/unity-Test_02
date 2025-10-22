c8ed81183a6bae9126473ba873750a853795bb08
swj01120-ship-it <swj01120@gmail.com>
2025-10-22 15:56:10 +0900

    리턴 레드 수정

--- Assets/He.cs
+++ Assets/He.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class He : MonoBehaviour
 {
+    SpriteRenderer mySR;
     // Start is called before the first frame update
     void Start()
     {
+        mySR = GetComponent<SpriteRenderer>();        
         Debug.Log("hehehehe");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            change_color();
+        }
         
+    }
+    void change_color()
+    {
+        mySR.color = Color.red;
     }
 }
