ba7a2b8beaa6bf7ec33843ed30928c779f5f2de4
swj01120-ship-it <swj01120@gmail.com>
2025-10-23 09:55:50 +0900

    ㅇㄷㅇㅌ

--- Assets/Helloworld.cs
+++ Assets/Helloworld.cs
@@ -4,24 +4,45 @@ using UnityEngine;
 
 public class Helloworld : MonoBehaviour
 {
+    Vector3 myVector = Vector3.zero;
+    Rigidbody2D myRigid;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Melong");
-        Debug.Log("Coffee");
-        Debug.Log("aaa");
-
-        Debug.Log("bbb");
-        Debug.Log("ccc");
-        Debug.Log("ddd");
-
-        Debug.Log("Ãß°¡");
+        myRigid = GetComponent<Rigidbody2D>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            myVector=Vector3.right;
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            myVector=Vector3.left;
+        }
+        else
+        {
+            myVector=Vector3.zero;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigid.AddForce(Vector3.up * 300.0f);
+        }
+
+        transform.Translate(myVector*3.0f*Time.deltaTime);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if(collision.gameObject.CompareTag("Floor"))
+        {
+
+        }
         
     }
 }
--- Assets/main.cs
+++ Assets/main.cs
@@ -47,8 +47,8 @@ public class main : MonoBehaviour
 
             }
         }
-        mySR.color  -= mycolor;
-        yield return null;
+        
+        
     }
     
 }
